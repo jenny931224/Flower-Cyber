129093c5c255c6ae7ff147919214abea2a814ab3
jenny931224 <93jani.rajakaru@gmail.com>
2020-12-05 12:09:46 +0530

    initial commit

---
+++ src/main/java/com/flower/cyber/flowercyber/FlowerCyberApplication.java
@@ -0,0 +1,13 @@
+package com.flower.cyber.flowercyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerCyberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerCyberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flower/cyber/flowercyber/FlowerCyberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flower.cyber.flowercyber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerCyberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
